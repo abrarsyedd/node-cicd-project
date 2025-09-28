922d0f15fdea5a59fd1b6aeeb0e11698a74d3341
Abrar Syed <abrarsyedd@gmail.com>
2025-09-28 21:17:04 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,17 +1,11 @@
-// app/index.js
 const express = require('express');
 const app = express();
+const port = 3000;
 
-// Use environment variables or default to 0.0.0.0 and 3000
-const HOST = process.env.HOST || '0.0.0.0'; 
-const PORT = process.env.PORT || 3000;
-
-// Simple route handler
 app.get('/', (req, res) => {
-  res.send('<h1>Node.js CI/CD App is Running!</h1>');
+  res.send('Hello from the Node.js App! This was built with CI/CD, Thank you');
 });
 
-// Start the server, listening on all interfaces (HOST: 0.0.0.0)
-app.listen(PORT, HOST, () => {
-  console.log(`Server is listening on http://${HOST}:${PORT}`);
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
 });
